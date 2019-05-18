362f0fe5e692306e51ff0ed9855b68bc7f7fca95
Alvin Seyidzade <alvinseyidzade@gmail.com>
2019-05-18 01:51:03 -0700

    Fix

--- home/models.py
+++ home/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Data(models.Model):
-    ad_soyad = models.CharField(max_length=100, null=True, blank=True)
-    yasadigi_region = models.CharField(max_length=100, null=True, blank=True)
-    profili = models.CharField(max_length=100, null=True, blank=True)
-    elaqenomresi = models.CharField(max_length=100, null=True, blank=True)
+    ad_soyad = models.CharField(max_length=1500, null=True, blank=True)
+    yasadigi_region = models.CharField(max_length=1500, null=True, blank=True)
+    profili = models.CharField(max_length=1500, null=True, blank=True)
+    elaqenomresi = models.CharField(max_length=1500, null=True, blank=True)
     image = models.ImageField(verbose_name='Şəkil', null=True, blank=True)
-    problemler = models.CharField(max_length=100, null=True, blank=True)
-    veziyyetsablonu = models.CharField(max_length=100, null=True, blank=True)
-    seher = models.CharField(max_length=100, null=True, blank=True)
-    kend = models.CharField(max_length=100, null=True, blank=True)
+    problemler = models.CharField(max_length=1500, null=True, blank=True)
+    veziyyetsablonu = models.CharField(max_length=1500, null=True, blank=True)
+    seher = models.CharField(max_length=1500, null=True, blank=True)
+    kend = models.CharField(max_length=1500, null=True, blank=True)
 
     def __str__(self):
         return self.ad_soyad
